3b4ebd10b3abbc77e80f03b3ef0fa73925858e39
FortunaGiovanni <giovannifortuna7@gmail.com>
2024-03-14 08:56:49 +0100

    versione0:errata

---
+++ src/FortunaGiovanniClass.java
@@ -0,0 +1,45 @@
+/**
+ * Classe per calcolare una produttoria:
+ * Fortuna Giovanni
+ 
+ * 
+ * @author 4a
+ *
+ */
+public class FortunaGiovanniClass {
+	/**
+	 *  Costruttore: 
+	 * 
+	 */
+	public FortunaGiovanniClass() {
+		
+	}
+	/**
+	 * 
+	 * @param s indice iniziale
+	 * @param f indice finale
+	 * @return risultato finale
+	 */
+	public int fortunaMet(int s, int f) {
+		int r=0;
+		for(int i=1; i<f; i++) {
+			r=r+r;
+		}
+		return r;
+		}
+	/**
+	 * @return toString
+	 */
+	public String toString() {
+		return " ";
+	}
+	/**
+	 * 
+	 * @param args Test di classe
+	 */
+	public static void main(String[] args) {
+		FortunaGiovanniClass p=new FortunaGiovanniClass();
+		System.out.println("il risultato è: "+p.fortunaMet(5, 7));
+	}
+	}
+
